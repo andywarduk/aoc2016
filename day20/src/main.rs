a586443f66b9b314c37c8ab9ae9da8042efb5ca0
Andy <andy.ward.uk@gmail.com>
2021-01-09 23:43:23 +0000

    Day 20

---
+++ day20/src/main.rs
@@ -0,0 +1,98 @@
+use std::{cmp, fs::File, io::{BufRead, BufReader}};
+use memmap::Mmap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let lines = load_input("input20.txt")?;
+
+    let ranges = parse_ranges(&lines);
+
+    part1(&ranges);
+    part2(&ranges);
+
+    Ok(())
+}
+
+fn part1(ranges: &Vec<Range>) {
+    let mut lowest: u32 = 0;
+
+    for r in ranges {
+        if r.lo > lowest {
+            break
+        }
+        lowest = cmp::max(lowest, r.hi + 1);
+    }
+
+    println!("Lowest ip is {} (part 1)", lowest);
+}
+
+fn part2(ranges: &Vec<Range>) {
+    let mut lowest: u32 = 0;
+    let mut allowed: u32 = 0;
+
+    for r in ranges {
+        if r.lo > lowest{
+            allowed += r.lo - lowest;
+        }
+        
+        lowest = if r.hi == u32::MAX {
+            u32::MAX
+        } else {
+            cmp::max(lowest, r.hi + 1)
+        }
+    }
+
+    println!("Number of allowed ips is {} (part 2)", allowed);
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Range {
+    lo: u32,
+    hi: u32
+}
+
+impl Range {
+    fn parse(line: &String) -> Range {
+        let mut split = line.split("-");
+
+        Range {
+            lo: split.next().unwrap().parse::<u32>().unwrap(),
+            hi: split.next().unwrap().parse::<u32>().unwrap(),
+        }
+    }
+}
+
+fn parse_ranges(lines: &Vec<String>) -> Vec<Range> {
+    let mut ranges: Vec<Range> = lines.iter().map(Range::parse).collect();
+    
+    ranges.sort();
+
+    ranges
+}
+
+fn load_input(file: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    // Open the file
+    let file = File::open(file)?;
+
+    // Memory map it
+    let mmap = unsafe { Mmap::map(&file)? };
+
+    // Drop the file
+    drop(file);
+
+    // Create buf reader for mmapped file
+    let buf_reader = BufReader::new(mmap.as_ref());
+
+    // Create lines vector
+    let mut lines = Vec::new();
+
+    // Iterate lines
+    for line_res in buf_reader.lines() {
+        let line = line_res?;
+
+        if line != "" {
+            lines.push(line);
+        }
+    }
+
+    Ok(lines)
+}
